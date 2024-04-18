ec9d97d3a0f4fcd05b1c8073359124c95f7dce06
NickFalletta <nickyny29@optonline.net>
2024-04-18 14:25:45 -0400

    total revamp!

--- src/tagging-q.js
+++ src/tagging-q.js
@@ -10,8 +10,8 @@ export class TaggingQ extends DDD {
 
   constructor() {
     super();
-    this.title = ""
-    this.questions = ["1","2","3"];
+    this.title = "What color is the sky?"
+    this.questions = ["blue", "red", "green", "purple"];
     this.answers = [];
     this.draggedIndex;
     this.draggedFrom;
@@ -35,7 +35,6 @@ export class TaggingQ extends DDD {
         width: 975px;
         display: flex;
         border: var(--ddd-theme-default-nittanyNavy) 20px outset;
-
         flex-direction: column;
         overflow: hidden;
       }
@@ -57,10 +56,15 @@ export class TaggingQ extends DDD {
         font-family: "Press Start 2P", system-ui;
         overflow-wrap: break-word;
         margin: var(--ddd-spacing-3);
+        width: 900px;
+        height: 200px;
+
+        display: flex;
+        flex-wrap: wrap;
       }
 
       .answer-box {
-        padding: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-4);
         color: var(--ddd-theme-default-opportunityGreen);
         background-color: var(--ddd-theme-default-futureLime);
         border: var(--ddd-theme-default-opportunityGreen) 5px dashed;
@@ -76,99 +80,125 @@ export class TaggingQ extends DDD {
 
       .answers-wrapper,
       .choices-wrapper {
-        display: flex;
-        margin: 8px;
-        justify-content: top;
-        align-items: top; 
+        margin: var(--ddd-spacing-1);
       }
 
       .answers,
       .choices {
-        display: flex;
-        font-size: 15px;
+        font-size: 25px;
         background-color: black;
         padding: var(--ddd-spacing-2);
-        border: white 2px solid;
+        border: white 5px solid;
+        color: white;
+      }
+
+      .answers:hover,
+      .choices:hover {
+        border-color: pink;
+        color: pink;
+      }
+
+      .answer-box.hovered,
+      .question-box.hovered {
+        border: pink 5px dashed;
+        color: pink;
+      }
+
+      .answers:focus,
+      .answers:hover,
+      .choices:focus,
+      .choices:hover{
+        border: pink 5px solid;
+        color: pink;
       }
     `;
   }
 
   firstUpdated() {
-    this.createQuestion();
+    super.firstUpdated();
+    const answerBoxes = this.shadowRoot.querySelectorAll('.answer-box');
+    const questionBoxes = this.shadowRoot.querySelectorAll('.question-box');
+
+    answerBoxes.forEach(answerBox => {
+      answerBox.addEventListener('dragstart', (e) => this.dragStart(e));
+      answerBox.addEventListener('dragover', (e) => this.dragOver(e));
+      answerBox.addEventListener('dragenter', (e) => this.dragEnter(e));
+      answerBox.addEventListener('dragleave', (e) => this.dragLeave(e));
+      answerBox.addEventListener('drop', (e) => this.drop(e, 'answer-box'));
+    });
+
+    questionBoxes.forEach(questionBox => {
+      questionBox.addEventListener('dragstart', (e) => this.dragStart(e));
+      questionBox.addEventListener('dragover', (e) => this.dragOver(e));
+      questionBox.addEventListener('dragenter', (e) => this.dragEnter(e));
+      questionBox.addEventListener('dragleave', (e) => this.dragLeave(e));
+      questionBox.addEventListener('drop', (e) => this.drop(e, 'question-box'));
+    });
   }
 
-  allowDrop(e) {
-      e.preventDefault();
+  dragOver(e) {
+    e.preventDefault();
   }
 
-  drag(index, origin) {
-      this.draggedIndex = index;
-      this.draggedFrom = origin;
+  dragStart(e) {
+    this.draggedIndex = parseInt(e.target.dataset.index);
+    this.draggedFrom = e.target.dataset.origin;
+  }
+
+  dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add('hovered');
+  }
+
+  dragLeave(e) {
+    e.target.classList.remove('hovered');
   }
 
   drop(e, target) {
-      e.preventDefault();
+    e.preventDefault();
 
-      if (target === 'answer-box') {
-          if (this.draggedFrom != 'answer-box') {
-              this.answers.push(this.questions[this.draggedIndex]);
-              this.questions.splice(this.draggedIndex, 1);
-          }
-      } else if (target === 'question-box') {
-          if (this.draggedFrom != 'question-box') {
-              this.questions.push(this.answers[this.draggedIndex]);
-              this.answers.splice(this.draggedIndex, 1);
-          }
+    e.target.classList.remove('hovered');
+
+    if (target === 'answer-box') {
+      if (this.draggedFrom != 'answer-box') {
+        this.answers.push(this.questions[this.draggedIndex]);
+        this.questions.splice(this.draggedIndex, 1);
       }
+    } else if (target === 'question-box') {
+      if (this.draggedFrom != 'question-box') {
+        this.questions.push(this.answers[this.draggedIndex]);
+        this.answers.splice(this.draggedIndex, 1);
+      }
+    }
 
-      this.requestUpdate();
+    this.requestUpdate();
   }
 
-  createQuestion() {
-    fetch("questions.json")
-        .then(d => d.json())
-        .then(data => {
-            this.title = data.title;
-            this.questions = data.questions.map(question => question.answer);
-            this.answers = data.questions.map(question => question.answer);
-        })
-        .catch(error => {
-            // window.alert(error);
-        });
-}
-
-  
-render() {
-
-  return html`
+  render() {
+    return html`
       <div class="project2">
-          <div class="background">
-              <div class="question-heading">Question: ${this.title}</div>
-
-              <div class="answer-box" @drop="${(e) => this.drop(e, 'answer-box')}" @dragover="${this.allowDrop}">Drag and Drop Answer
-                  ${this.answers.map((answer, index) => html`
-
-                  <div class="answers-wrapper">
-                      <div class="answers" draggable="true" @dragstart="${() => this.drag(index, 'answer-box')}">${answer}</div>
-                  </div>
-                  `)}
+        <div class="background">
+          <div class="question-heading">Question: ${this.title}</div>
+          <div class="answer-box">
+            Drag and Drop Answer
+            ${this.answers.map((answer, index) => html`
+              <div class="answers-wrapper">
+                <div class="answers" draggable="true" data-index="${index}" data-origin="answer-box">${answer}</div>
               </div>
-
-              <div class="question-box" @drop="${(e) => this.drop(e, 'question-box')}" @dragover="${this.allowDrop}">
-
-              ${this.questions.map((question, index) => html`
-
-                  <div class="choices-wrapper">
-                      <div class="choices" draggable="true" @dragstart="${() => this.drag(index, 'question-box')}">${question}</div>
-                  </div>
-
-              `)}
-
-              </div>
+            `)}
           </div>
+          <div class="question-box">
+            ${this.questions.map((question, index) => html`
+              <div class="choices-wrapper">
+                <div class="choices" draggable="true" data-index="${index}" data-origin="question-box">${question}</div>
+              </div>
+            `)}
+          </div>
+        </div>
       </div>
-  `;
-}
+    `;
+  }
+
 
   static get properties() {
     return {
